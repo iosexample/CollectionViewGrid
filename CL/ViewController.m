0fbc6efc126fbc03c80a3ee5c0056d3a0dd0b787
Dong <donly@magicalboy.com>
2015-11-04 09:59:10 +0800

    fixed 1 pix margin bug

--- CL/ViewController.m
+++ CL/ViewController.m
@@ -12,16 +12,57 @@
 
 @end
 
+static NSString * const reuseIdentifier = @"CategoryItemCell";
+
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    UICollectionViewFlowLayout *layout = [[UICollectionViewFlowLayout alloc] init];
+    
+    layout.minimumInteritemSpacing = -1.f/[[UIScreen mainScreen] scale];
+    layout.minimumLineSpacing = -1.f/[[UIScreen mainScreen] scale];
+
+    [self.collectionView setCollectionViewLayout:layout];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark <UICollectionViewDataSource>
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
+    return 1;
+}
+
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return 19;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:reuseIdentifier forIndexPath:indexPath];
+    cell.backgroundColor = [UIColor grayColor];
+    cell.layer.borderWidth = 1.f/[[UIScreen mainScreen] scale];
+    return cell;
+}
+
+#pragma mark <UICollectionViewDelegate>
+- (CGSize)collectionView:(UICollectionView *)collectionView
+                  layout:(UICollectionViewLayout*)collectionViewLayout
+  sizeForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    CGFloat SCREEN_WIDTH = CGRectGetWidth([[UIScreen mainScreen] bounds]);
+    CGFloat width = ceilf((SCREEN_WIDTH)/3.f), height = width;
+    // because 320/3 < 107 + 107 + 107, there was 1 pix margin after 2nd colomn
+    return CGSizeMake(width, height);
+}
+
+- (UIEdgeInsets)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout insetForSectionAtIndex:(NSInteger)section{
+    return UIEdgeInsetsMake(0, 0, 0, 0);
 }
 
 @end
